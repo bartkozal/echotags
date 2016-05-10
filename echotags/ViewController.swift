c03cf84d30ca5e722ff2df0c98ebea9ef20a6498
Bartlomiej Kozal <bkzl@me.com>
2016-05-10 15:19:08 +0200

    Basic implementation of the interface

--- echotags/ViewController.swift
+++ echotags/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func unwindToViewController(sender: UIStoryboardSegue) {}
 
 }
-
