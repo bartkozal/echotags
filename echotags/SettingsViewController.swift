a7d18ce15503178517c7bc5e499c666d655f621b
Bartlomiej Kozal <bkzl@me.com>
2016-05-14 23:14:38 +0200

    Change checkbox by tapping on label

--- echotags/SettingsViewController.swift
+++ echotags/SettingsViewController.swift
@@ -11,32 +11,15 @@ import BEMCheckBox
 
 class SettingsViewController: UIViewController {
     @IBOutlet weak var checkbox: BEMCheckBox!
-    @IBOutlet weak var checkbox2: BEMCheckBox!
+    
+    @IBAction func touchCheckboxLabel(sender: UIButton) {
+        checkbox.setOn(!checkbox.on, animated: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         checkbox.onAnimationType = .Bounce
         checkbox.offAnimationType = .Bounce
-        
-        checkbox2.onAnimationType = .Bounce
-        checkbox2.offAnimationType = .Bounce
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
