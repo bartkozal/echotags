c6447fb094c64cd726082fd7b141e0fd79253af4
Bartlomiej Kozal <bkzl@me.com>
2016-05-26 00:13:51 +0200

    Bundle database with POIs

--- echotags/Data.swift
+++ echotags/Data.swift
@@ -10,20 +10,4 @@ import RealmSwift
 
 class Data {
     static let db = try! Realm()
-    
-    static func populate() {
-        let point = Point(value: ["Upstream Gallery", 52.369078, 4.89752539999995, ""])
-        let category = Category(value: ["Art", true])
-        let marker = Marker(value: [point, category])
-        
-        try! db.write {
-            db.add(point)
-            db.add(category)
-            db.add(marker)
-        }
-    }
-    
-    static func isPopulateRequired() -> Bool {
-        return db.objects(Marker).count < 1
-    }
 }
