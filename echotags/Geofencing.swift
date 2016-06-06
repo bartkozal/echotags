6372c9c0b2d5f6dafea48c204b8b32e4ad08def8
Bartlomiej Kozal <bkzl@me.com>
2016-06-06 21:10:10 +0200

    Monitor all points

--- echotags/Geofencing.swift
+++ echotags/Geofencing.swift
@@ -11,7 +11,7 @@ import CoreLocation
 
 class Geofencing {
     var manager: CLLocationManager
-
+    
     struct Defaults {
         static let coordinate = CLLocationCoordinate2D(latitude: 52.371413, longitude: 4.897451)
         static let zoomLevel = 13.5
@@ -22,7 +22,7 @@ class Geofencing {
         static let northEast = CLLocationCoordinate2D(latitude: 52.42857381779567, longitude: 5.009148437500016)
         static let southWest = CLLocationCoordinate2D(latitude: 52.28064348619208, longitude: 4.724184204101562)
     }
-
+    
     init() {
         manager = CLLocationManager()
         manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
@@ -44,10 +44,15 @@ class Geofencing {
         }
     }
     
-    func monitorNearestPoints() {
-        // TODO: missing neareast point localization
+    func monitorNearestPointsTo(source: CLLocationCoordinate2D) {
         
-        if let point = Point.findByTitle("Upstream Gallery") {
+        // Returns 20 nearest points to user location. 20 is max limit of monitored regions for iOS9
+        let nearestPoints = Marker.visible().sort { p1, p2 in
+            distanceBetween(source, target: CLLocationCoordinate2D(latitude: (p1 as! Point).latitude, longitude: (p1 as! Point).longitude)) < distanceBetween(source, target: CLLocationCoordinate2D(latitude: (p2 as! Point).latitude, longitude: (p2 as! Point).longitude))
+        }[0...20]
+
+        for point in nearestPoints {
+            let point = point as! Point
             let coordinate = CLLocationCoordinate2D(latitude: point.latitude, longitude: point.longitude)
             let region = circularRegionFrom(coordinate, withAudio: point.audio)
             manager.startMonitoringForRegion(region)
@@ -56,5 +61,21 @@ class Geofencing {
     
     private func circularRegionFrom(coordinate: CLLocationCoordinate2D, withAudio audio: String) -> CLCircularRegion {
         return CLCircularRegion(center: coordinate, radius: Defaults.pointRadius, identifier: audio)
+    }
+    
+    private func distanceBetween(source: CLLocationCoordinate2D, target: CLLocationCoordinate2D) -> Double {
+        let sourceLatRad = source.latitude * M_PI / 180
+        let sourceLngRad = source.longitude * M_PI / 180
+        let targetLatRad = target.latitude * M_PI / 180
+        let targetLngRad = target.longitude * M_PI / 180
+        
+        let deltaLat = targetLatRad - sourceLatRad
+        let deltaLng = targetLngRad - sourceLngRad
+        
+        let a = sin(deltaLat/2) * sin(deltaLat/2) + sin(deltaLng/2) * sin(deltaLng/2) * cos(sourceLatRad) * cos(targetLatRad)
+        let c = 2 * asin(sqrt(a))
+        let r = 6372.8
+        
+        return r * c
     }
 }
