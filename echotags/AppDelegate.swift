5744248870daf9c78be7dc225dd4635ed2d80cc3
Bartlomiej Kozal <bkzl@me.com>
2016-06-09 14:35:45 +0200

    Add first launch only actions

--- echotags/AppDelegate.swift
+++ echotags/AppDelegate.swift
@@ -17,7 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
-        Database().copyToFileSystem()
+        if UserDefaults.isFirstLaunch() {
+            Database().copyToFileSystem()
+        }
         
         return true
     }
@@ -43,7 +45,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-    
-    
 }
-
