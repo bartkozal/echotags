c399f725b570f13823d2dd1803c6a1a3e69c51b4
Bartlomiej Kozal <bkzl@me.com>
2016-06-16 16:50:27 +0200

    Extract queries to new file

--- Echotags/Database/Schema.swift
+++ Echotags/Database/Schema.swift
@@ -8,10 +8,6 @@
 
 import RealmSwift
 
-// Point has many Categories through Markers
-// Category has many Points through Markers
-// Point has many Triggers
-// Trigger belongs to Point
 
 class Point: Object {
     dynamic var title = ""
@@ -20,38 +16,16 @@ class Point: Object {
     dynamic var audio = ""
     dynamic var visited = false
     let triggers = List<Trigger>()
-    
-    static func findByTitle(title: String) -> Point? {
-        return Database().db.objects(Point).filter("title = %@", title).first ?? nil
-    }
 }
 
 class Marker: Object {
     dynamic var point: Point?
     dynamic var category: Category?
-    
-    static func visible() -> [Object] {
-        return Database().db.objects(Marker).filter("category.visible = true").uniqueObject("point")
-    }
 }
 
 class Category: Object {
     dynamic var name = ""
     dynamic var visible = true
-    
-    static func all() -> Results<Category> {
-        return Database().db.objects(Category)
-    }
-    
-    static func findByName(name: String) -> Category? {
-        return Database().db.objects(Category).filter("name = %@", name).first ?? nil
-    }
-    
-    func updateVisibility(value: Bool) {
-        try! Database().db.write {
-            visible = value
-        }
-    }
 }
 
 class Trigger: Object {
@@ -59,8 +33,4 @@ class Trigger: Object {
     dynamic var latitude = 0.0
     dynamic var longitude = 0.0
     let point = LinkingObjects(fromType: Point.self, property: "triggers")
-    
-    static func findById(id: String) -> Trigger? {
-        return Database().db.objects(Trigger).filter("id = %@", id).first ?? nil
-    }
 }
