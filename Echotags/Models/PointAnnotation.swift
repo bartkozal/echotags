30adcd399f4b31d8ab824d4ad2863fdaa2e03e79
Bartlomiej Kozal <bkzl@me.com>
2016-06-27 20:39:23 +0200

    Allow to play recording from annotation

---
+++ Echotags/Models/PointAnnotation.swift
@@ -0,0 +1,14 @@
+//
+//  PointAnnotation.swift
+//  Echotags
+//
+//  Created by bkzl on 27/06/16.
+//  Copyright © 2016 Bartłomiej Kozal. All rights reserved.
+//
+
+import Mapbox
+
+class PointAnnotation: MGLPointAnnotation {
+    var audio: String?
+    var marker: String?
+}
