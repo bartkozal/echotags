f1461a6badec7d0bdf872b5b1445a50f13a14ff8
Bartlomiej Kozal <bkzl@me.com>
2016-07-20 01:09:23 +0200

    Clean up alerts

--- Echotags/Views/Alert.swift
+++ Echotags/Views/Alert.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 struct Alert {
-    var vc: UIViewController
     
-    func accessToLocationBackgroundDenied() {
+    private static func singleAction(title: String, message: String) -> UIAlertController {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let confirmAction = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+        
+        alertController.addAction(confirmAction)
+        
+        return alertController
+    }
+    
+    static func outOfBounds() -> UIAlertController {
+        return singleAction(
+            "Out of bounds",
+            message: "Seems you are out of Amsterdam. You can still review markers but navigation mode is unavailable."
+        )
+    }
+    
+    static func twitterUnavailable() -> UIAlertController {
+        return singleAction(
+            "Account is unavailable",
+            message: "You are not logged in to your Twitter account."
+        )
+    }
+    
+    static func facebookUnavailable() -> UIAlertController {
+        return singleAction(
+            "Account is unavailable",
+            message: "You are not logged in to your Facebook account."
+        )
+    }
+    
+    static func accessToLocationBackgroundDenied()  -> UIAlertController {
         let alertController = UIAlertController(
             title: "You didn't allow to access background location",
-            message: "In order to play audio messages, please tap on \"Settings\" and set location access to \"Always\".",
+            message: "In order to play audio messages, please tap on \"Settings\" and set location access to \"While Using the App\".",
             preferredStyle: .Alert)
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
@@ -26,42 +55,6 @@ struct Alert {
             }
         }
         alertController.addAction(openAction)
-        
-        vc.presentViewController(alertController, animated: true, completion: nil)
-    }
-    
-    func twitterUnavailable() {
-        let alertController = okAlertController(
-            "Account is unavailable",
-            message: "You are not logged in to your Twitter account."
-        )
-        
-        vc.presentViewController(alertController, animated: true, completion: nil)
-    }
-    
-    func facebookUnavailable() {
-        let alertController = okAlertController(
-            "Account is unavailable",
-            message: "You are not logged in to your Facebook account."
-        )
-        
-        vc.presentViewController(alertController, animated: true, completion: nil)
-    }
-    
-    func outOfBounds() {
-        let alertController = okAlertController(
-            "Out of bounds",
-            message: "Seems you are out of Amsterdam. You can still review markers but navigation mode is unavailable."
-        )
-        
-        vc.presentViewController(alertController, animated: true, completion: nil)
-    }
-    
-    private func okAlertController(title: String, message: String) -> UIAlertController {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
-        let okAction = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
-        
-        alertController.addAction(okAction)
         
         return alertController
     }
