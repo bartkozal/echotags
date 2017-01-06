838567285901d049345adb17d1be40f5712f8ffb
Bartlomiej Kozal <bkzl@me.com>
2017-01-06 02:21:13 +0100

    Change look of the discover button

--- Echotags/Views/Colors.swift
+++ Echotags/Views/Colors.swift
@@ -12,4 +12,8 @@ extension UIColor {
     static var brandColor: UIColor {
         return UIColor(red: 0.22, green: 0.26, blue: 0.35, alpha: 1.0)
     }
+
+    static var positiveColor: UIColor {
+        return UIColor(red: 0.23, green: 0.65, blue: 0.27, alpha: 1.0)
+    }
 }
---
+++ Echotags/Views/DiscoveryButton.swift
@@ -0,0 +1,47 @@
+//
+//  DiscoveryButton.swift
+//  echotags
+//
+//  Created by bkzl on 06/01/2017.
+//  Copyright © 2017 Bartłomiej Kozal. All rights reserved.
+//
+
+import UIKit
+
+class DiscoveryButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setStyles()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setStyles()
+    }
+
+    private func setStyles() {
+        self.backgroundColor = .positiveColor
+        layoutIfNeeded()
+        layer.borderColor = UIColor.white.cgColor
+        layer.borderWidth = 3
+        layer.cornerRadius = bounds.width / 2
+        layer.shadowOffset = CGSize(width: 0, height: 1)
+        layer.shadowRadius = 2
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.5
+    }
+
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.backgroundColor = .brandColor
+                })
+            } else {
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.backgroundColor = .positiveColor
+                })
+            }
+        }
+    }
+}
